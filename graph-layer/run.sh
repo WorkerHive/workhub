c1ef63e5dec382591b1a5565b39a347f3f54371f
Ross Leitch <professional.balbatross@gmail.com>
2020-12-18 17:09:22 +1300

    Updating site

--- graph-layer/run.sh
+++ graph-layer/run.sh
@@ -1,3 +1,6 @@
-mkdir ./greenlock.d
-gomplate -f greenlock.template -o ./greenlock.d/config.json
+FILE=./greenlock.d/config.json
+if [ ! -f "$FILE" ]; then
+  mkdir ./greenlock.d
+  gomplate -f greenlock.template -o ./greenlock.d/config.json
+fi
 git reset --hard HEAD && git pull && yarn install && node index.js
