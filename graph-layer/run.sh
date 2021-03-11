36c5547395ce24d75d6ed38ddd4dff51e94753f9
Ross Leitch <professional.balbatross@gmail.com>
2021-03-11 14:55:37 +1300

    Shrinking graph docker build

--- graph-layer/run.sh
+++ graph-layer/run.sh
@@ -1,6 +1,1 @@
-FILE=./greenlock.d/config.json
-if [ ! -f "$FILE" ]; then
-  mkdir ./greenlock.d
-  gomplate -f greenlock.template -o ./greenlock.d/config.json
-fi
-git pull && npm install && npx lerna bootstrap --scope workhub-server --include-dependencies && npm run start:server
+workhub start -c /data/workhub
